e6211b6119190bb615e498dd16e22a97f71eff9a
Amir Hallaji <a.hallaji.b@gmail.com>
2022-08-23 19:50:37 +0430

    feat: add config

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,56 @@
 package config
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/pkg/errors"
+	"github.com/spf13/viper"
+)
+
+type (
+	Config struct {
+		API API `mapstructure:"api"`
+	}
+
+	API struct {
+		Port string `mapstructure:"port"`
+	}
+
+	Logger struct {
+		Level string `mapstructure:"level"`
+	}
+
+	Services struct {
+		Seer Seer `mapstructure:"seer"`
+	}
+
+	Seer struct {
+		Timeout            time.Duration `mapstructure:"timeout"`
+		MaxRetry           int           `mapstructure:"max_retry"`
+		MaxIdleConsPerHost int           `mapstructure:"max_idle_cons_per_host"`
+		MaxIdleCons        int           `mapstructure:"max_idle_cons"`
+		MaxConsPerHost     int           `mapstructure:"max_cons_per_host"`
+		URL                string        `mapstructure:"url"`
+		AuthUser           string        `mapstructure:"auth_user"`
+		AuthPass           string        `mapstructure:"auth_pass"`
+		Enable             bool          `mapstructure:"enable"`
+	}
+)
+
+func Load() (cfg *Config, err error) {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yml")
+	viper.AddConfigPath("./config")
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err != nil {
+		return cfg, fmt.Errorf("failed to load config: %w", err)
+	}
+
+	if err = viper.Unmarshal(&cfg); err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	return cfg, nil
+}
