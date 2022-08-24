e4e424f7bc5e30f186b01d9e29197324e2e3dab6
Amir Hallaji <a.hallaji.b@gmail.com>
2022-08-24 22:34:37 +0430

    feat: create new client

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,1 +1,19 @@
-package api
+package main
+
+import (
+	"github.com/AJob-Recommender/base-api/internal/api"
+	"github.com/AJob-Recommender/base-api/internal/config"
+	"github.com/AJob-Recommender/base-api/pkg/logger"
+)
+
+func main() {
+	cfg, err := config.Load()
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	log, err := logger.New()
+
+	api.Serve(cfg, log)
+}
