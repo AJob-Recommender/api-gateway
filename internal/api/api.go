a36efcaa16f1595b96e4413681d4582ebc934e2f
Amir Hallaji <a.hallaji.b@gmail.com>
2022-08-25 22:53:07 +0430

    feat: ad cors config

--- internal/api/api.go
+++ internal/api/api.go
@@ -5,15 +5,24 @@ import (
 	"github.com/AJob-Recommender/base-api/internal/handler"
 	"github.com/AJob-Recommender/base-api/internal/services/seer"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"go.uber.org/zap"
 )
 
 func Serve(cfg *config.Config, log *zap.SugaredLogger) {
 	fib := fiber.New()
 
-	seerClient := seer.NewSeer(cfg, log)
+	corsConfig := cors.ConfigDefault
+	corsConfig.AllowHeaders = "*"
+	corsConfig.AllowCredentials = true
+	fib.Use(cors.New(corsConfig))
 
+	seerClient := seer.NewSeer(cfg, log)
 	h := handler.NewHandler(cfg, log, seerClient)
 
 	route(fib, h)
+
+	if err := fib.Listen(cfg.API.Port); err != nil {
+		log.Error(err.Error())
+	}
 }
